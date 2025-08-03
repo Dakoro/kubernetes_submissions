314d09a48b1098bc320f408c67395d637fddbf9d
Dakoro <david.korobetski@gmail.com>
2025-08-03 13:28:43 +0200

    feat: finish exercice 1.4

--- todo/deploy-to-k3d.sh
+++ todo/deploy-to-k3d.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Create k3d cluster
-k3d cluster create log-output-cluster --port "8080:80@loadbalancer"
+k3d cluster create todo-app-cluster --port "8000:80@loadbalancer"
 
 # Build and import Docker image into k3d
-docker build -t log-output-app:latest .
-k3d image import log-output-app:latest -c log-output-cluster
+docker build -t todo-app:latest .
+k3d image import todo-app:latest -c todo-app-cluster
 
 # Apply Kubernetes manifests
-kubectl apply -f manifests/k3d-deployment.yaml
+kubectl apply -f manifests/deployment.yaml
 
 # Check deployment status
 kubectl get pods
 
-echo "To view logs: kubectl logs -f deployment/log-output-app"
+echo "To view logs: kubectl logs -f deployment/todo-app"
 echo "To delete cluster: make clean"
